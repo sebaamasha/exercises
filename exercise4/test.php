8311576637d925eaf3e15ce8c3c3a1a1ae845554
sebaamasha <127793243+sebaamasha@users.noreply.github.com>
2023-05-09 02:22:11 +0300

    Add files via upload

---
+++ exercise4/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>formbyme</title>
+    </head>
+    <body>
+        <h3>The color that you want: <?php echo $_GET["color"]; ?><br><br>
+        adding buttons: <?php echo $_GET["buttons"]; ?> <br><br>
+            Your coupon is: <?php echo $_GET["coupon"]; ?> <br><br>
+        </h3>
+    </body>
+</html>
